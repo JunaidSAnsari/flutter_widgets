bfcbce6691bd84aba84113b62fedd9e89afa5e93
JunaidSAnsari <thejunaidsaleem@gmail.com>
2022-06-04 19:38:44 +0500

    ListView.Separated & Card

--- lib/main.dart
+++ lib/main.dart
@@ -6,39 +6,85 @@ void main() {
       appBar: AppBar(
         title: Text("ListView Design"),
       ),
-      body: ListViewDesign(),
+      body: ListViewBuilder(),
     ),
   ));
 }
 
-class ListViewDesign extends StatelessWidget {
-  List<String> months = [
-    "Jan",
-    "Feb",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec"
-  ];
-  List <int> colourNo = [100,200,300,400,500,600,700,800,900,800,700,600];
+// class cardViewDesign extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Card(
+//       child: Column(
+//         children: [
+//           ListTile(
+//             leading: Icon(Icons.person_add),
+//             title: Text("Junaid"),
+//             subtitle: Text("Flutter App developer"),
+//             trailing: Icon(Icons.arrow_forward),
+//             onTap: () {},
+//           ),
+//           Row(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               TextButton(onPressed: () {}, child: Text("Add")),
+//               TextButton(onPressed: () {}, child: Text("Remove")),
+//             ],
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+class ListViewBuilder extends StatelessWidget {
+  // List<int> count = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
   @override
   Widget build(BuildContext context) {
-    //ListView Builder
-    return ListView.builder(
-        itemCount: months.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Container(
-            height: 70,
-            color: Colors.amber[colourNo[index]],
-            child: Center(child: Text("${months[index]}")),
-          );
-        });
+    return ListView.separated(
+      separatorBuilder: (BuildContext context, index) {
+        return Divider(
+          // height: 10,
+          thickness: 2,
+          indent: 25,
+          endIndent: 25,
+          color: Colors.green,
+        );
+      },
+      itemCount: 30,
+      itemBuilder: (BuildContext context, index) {
+        return Card(
+          // color: Colors.black12,
+          child: Column(
+            children: [
+              ListTile(
+                leading: Icon(Icons.person_add),
+                title: Text("Junaid"),
+                subtitle: Text("Flutter App developer"),
+                trailing: Icon(Icons.arrow_forward),
+                onTap: () {},
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Add",
+                        style:
+                            TextStyle(color: Colors.yellowAccent, fontSize: 20),
+                      )),
+                  TextButton(
+                      onPressed: () {},
+                      child: Text("Remove",
+                          style: TextStyle(color: Colors.red, fontSize: 20))),
+                ],
+              ),
+            ],
+          ),
+        );
+      },
+    );
   }
 }
