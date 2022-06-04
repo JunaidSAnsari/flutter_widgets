debbd3031f72828952ed05c47d3eca38c44a0aa9
JunaidSAnsari <thejunaidsaleem@gmail.com>
2022-06-04 13:31:39 +0500

    Some changes made

--- lib/main.dart
+++ lib/main.dart
@@ -15,17 +15,17 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  getImage() {
-    AssetImage assetImage = AssetImage("images/money.png");
-    Image image = Image(
-      image: assetImage,
-      height: 150,
-    );
-    return Container(
-      padding: EdgeInsets.only(top: 30, bottom: 30),
-      child: image,
-    );
-  }
+  // getImage() {
+  //   AssetImage assetImage = AssetImage("images/money.png");
+  //   Image image = Image(
+  //     image: assetImage,
+  //     height: 150,
+  //   );
+  //   return Container(
+  //     padding: EdgeInsets.only(top: 30, bottom: 30),
+  //     child: image,
+  //   );
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -35,38 +35,61 @@ class _MyAppState extends State<MyApp> {
       home: Scaffold(
         appBar: AppBar(
           leading: Icon(Icons.calculate),
-          title: Text("Interest Calculator", style: TextStyle(fontSize: 25),),
+          title: Text(
+            "Interest Calculator",
+            style: TextStyle(fontSize: 25),
+          ),
         ),
         body: Container(
           color: Colors.black87,
           child: Column(
             children: [
-              getImage(),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: TextField(
-                  decoration: InputDecoration(
-
-                    prefixIcon: Icon(Icons.money, color: Colors.white70,),
-                    labelText: "Principal",
-                    labelStyle: TextStyle(color: Colors.white70),
-                    hintText: "Principal",
-                    focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white70)),
-                    enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white70)),
-                  ),
-
+              Container(
+                padding: EdgeInsets.only(top: 30, bottom: 30),
+                child: Image.asset(
+                  "images/money.png",
+                  height: 150,
                 ),
               ),
               Padding(
                 padding: const EdgeInsets.all(10.0),
                 child: TextField(
                   decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.rate_review, color: Colors.white70,),
+                    prefixIcon: Icon(
+                      Icons.money,
+                      color: Colors.white70,
+                    ),
+                    labelText: "Principal",
+                    labelStyle: TextStyle(color: Colors.white70),
+                    hintText: "Principal",
+                    hintStyle: TextStyle(color: Colors.white60),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white70)
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white70)
+                    ),
+                  ),
+                    style: TextStyle(color: Colors.white70),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(
+                      Icons.rate_review,
+                      color: Colors.white70,
+                    ),
                     labelText: "Interest",
                     labelStyle: TextStyle(color: Colors.white70),
                     hintText: "Rate of Interest",
-                    focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white70)),
-                    enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white70)),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white70)
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white70)
+                    ),
                   ),
                 ),
               ),
@@ -77,12 +100,19 @@ class _MyAppState extends State<MyApp> {
                       padding: const EdgeInsets.all(10.0),
                       child: TextField(
                         decoration: InputDecoration(
-                          prefixIcon: Icon(Icons.telegram, color: Colors.white70,),
-                            labelText: "Term",
-                            labelStyle: TextStyle(color: Colors.white70),
-                            hintText: "Term",
-                          focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white70)),
-                          enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white70)),
+                          prefixIcon: Icon(
+                            Icons.telegram,
+                            color: Colors.white70,
+                          ),
+                          labelText: "Term",
+                          labelStyle: TextStyle(color: Colors.white70),
+                          hintText: "Term",
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white70)
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white70)
+                          ),
                         ),
                       ),
                     ),
@@ -92,12 +122,19 @@ class _MyAppState extends State<MyApp> {
                       padding: const EdgeInsets.all(10.0),
                       child: TextField(
                         decoration: InputDecoration(
-                            prefixIcon: Icon(Icons.currency_rupee, color: Colors.white70,),
-                            labelText: "Rupees",
-                            labelStyle: TextStyle(color: Colors.white70),
-                            hintText: "Rupees",
-                          focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white70)),
-                          enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white70)),
+                          prefixIcon: Icon(
+                            Icons.currency_rupee,
+                            color: Colors.white70,
+                          ),
+                          labelText: "Rupees",
+                          labelStyle: TextStyle(color: Colors.white70),
+                          hintText: "Rupees",
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white70)
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white70)
+                          ),
                         ),
                       ),
                     ),
@@ -111,20 +148,29 @@ class _MyAppState extends State<MyApp> {
                     Expanded(
                       child: ElevatedButton(
                           style: ElevatedButton.styleFrom(
-                              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(0)),
-                              primary: Colors.purple.shade600
-                          ),
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(0)
+                              ),
+                              primary: Colors.purple.shade600),
                           onPressed: () {},
-                          child: Text("Calculate", style: TextStyle(color: Colors.black, fontSize: 25),)),
+                          child: Text(
+                            "Calculate",
+                            style: TextStyle(color: Colors.black, fontSize: 25),
+                          )),
                     ),
                     Expanded(
                       child: ElevatedButton(
-                          style:
-                              ElevatedButton.styleFrom(
-                                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(0)),
-                                  primary: Colors.black),
+                          style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(0)
+                              ),
+                              primary: Colors.black),
                           onPressed: () {},
-                          child: Text("Reset", style: TextStyle(color: Colors.white70, fontSize: 25),)),
+                          child: Text(
+                            "Reset",
+                            style:
+                                TextStyle(color: Colors.white70, fontSize: 25),
+                          )),
                     ),
                     // Expanded(
                     //     child: ElevatedButton(
