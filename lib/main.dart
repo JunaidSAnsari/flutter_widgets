75be62d1b79eb9a8d863ec68b9795aee80061a01
JunaidSAnsari <thejunaidsaleem@gmail.com>
2022-06-13 19:36:31 +0500

    DropDown menu

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,44 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main (){
-  runApp(FloatingactionButton());
+void main() {
+  runApp(MaterialApp(
+    home: dropDownDesign(),
+  ));
 }
 
-class FloatingactionButton extends StatelessWidget {
+class dropDownDesign extends StatefulWidget {
+  const dropDownDesign({Key? key}) : super(key: key);
+
+  @override
+  State<dropDownDesign> createState() => _dropDownDesignState();
+}
+
+class _dropDownDesignState extends State<dropDownDesign> {
+  List<String> list = ["Select Country", "Pakistan", "India", "Bangladesh"];
+  String selectedCounry = "Select Country";
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text("Floating Action Button"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Drop Down Menu"),
+      ),
+      body: Center(
+        child: DropdownButton<String>(
+          items: list.map<DropdownMenuItem<String>>((String v) {
+            return DropdownMenuItem<String>(
+              child: Text(v),
+              value: v,
+            );
+          }).toList(),
+          value: selectedCounry,
+          onChanged: (String? newCountry) {
+            setState(() {
+              selectedCounry = newCountry!;
+            });
+          },
         ),
-        body: Center(
-          child: Text("Floating Action Button", style: TextStyle(fontSize: 30),),
-        ),
-        floatingActionButton: FloatingActionButton(
-            onPressed: (){
-            },
-          child: Icon(Icons.add),
-          backgroundColor: Colors.amber,
-        ),
-
-
       ),
     );
   }
