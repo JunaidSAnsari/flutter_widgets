4b5ef0df3b80b83feb98236d02507d808b5bd081
JunaidSAnsari <thejunaidsaleem@gmail.com>
2022-06-13 18:38:48 +0500

    Floating Action Button

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,30 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:shared_pref/AboutUs.dart';
-import 'package:shared_pref/PolicyPage.dart';
-import 'package:shared_pref/contactUs.dart';
 
 void main (){
-  runApp(MaterialApp(
-    home: DrawerDesign()
-  ));
+  runApp(FloatingactionButton());
 }
 
-class DrawerDesign extends StatelessWidget {
-  const DrawerDesign({Key? key}) : super(key: key);
+class FloatingactionButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: Text("Drawer Design"),),
-        drawer: Drawer(
-          child: ListView(
-            children: [
-              DrawerHeader(
-                  child: Text("Drawer Header"),
-                decoration: BoxDecoration(
-                  color: Colors.red
-                ),
-              ),
-              ListTile(
-                title: Text("Contact Us"),
-                onTap: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (context){
-                    return contactUs();
-                  }
-                  )
-                  );
-                },
-              ),
-              ListTile(
-                title: Text("Policy"),
-                onTap: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (context){
-                    return policyPage();
-                  })
-                  );
-                },
-              ),
-              ListTile(
-                title: Text("About Us"),
-                onTap: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (context){
-                    return aboutUs();
-                  })
-                  );
-                },
-              ),
-
-
-            ],
-          ),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Floating Action Button"),
         ),
-      body: Center(
-        child: Text("Drawer Navigation Design"),
-      ),
+        body: Center(
+          child: Text("Floating Action Button", style: TextStyle(fontSize: 30),),
+        ),
+        floatingActionButton: FloatingActionButton(
+            onPressed: (){
+            },
+          child: Icon(Icons.add),
+          backgroundColor: Colors.amber,
+        ),
 
+
+      ),
     );
   }
 }
